dc13f70667f5bc740a6dfc0cd18eda396e7c24a2
Tim Davies <tim.davies@atcogas.com.au>
2018-10-02 13:03:41 +0800

    Updated load file

--- notebooks/munging.py
+++ notebooks/munging.py
@@ -1,8 +1,19 @@
 import pandas as pd
 
 def load_data(data_file, sht_name, file_type):
-    if file_type == 'Excel':
-        df = pd.read_excel(io=data_file, sheet_name = sht_name)
-    if file_type == 'CSV' or 'csv':
-        df = pd.read_csv(data_file)
+    excel_filetypes = ['Excel', 'xls', 'XLS', 'xlsx']
+    csv_filetypes = ['CSV' or 'csv']
+    
+    if file_type in excel_filetypes
+        try:
+            df = pd.read_excel(io=data_file, sheet_name = sht_name)
+        except:
+            print ("Error opening file.")
+    
+    if file_type in csv_filetypes:
+        try:
+            df = pd.read_csv(data_file)
+        except:
+            print ("Error opening file.")
+    
     return df
